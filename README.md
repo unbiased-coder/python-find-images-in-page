c7ab7b4b6a2dc8b216160c4d13964946cb908c49
Unbiased Coder <info@unbiased-coder.com>
2022-05-23 17:09:09 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# python-find-images-in-page
 Python Extract Images From Webpage
+
+You can find the full guide with step by step instructions here:
+
+https://unbiased-coder.com/python-find-images-in-page/
---
+++ python-extract-images-page.py
@@ -0,0 +1,33 @@
+from telnetlib import TUID
+import requests
+
+from bs4 import BeautifulSoup
+
+page = 'https://www.google.com'
+
+
+def download_image(url):
+    if 'http' != url[:4]:
+        turl = page + url
+    else:
+        turl = url
+
+    print(f'Downloading file: {turl}')
+    res = requests.get(turl)
+    save_location = turl.split('/')[-1]
+    print (f'Saving file to: {save_location}')
+    with open(save_location, 'wb') as fd:
+        fd.write(res.content)
+    print (f'Successfully saved file')
+
+print (f'Downloading page: {page}')
+res = requests.get(page)
+
+print (f'Got back response: {res.status_code}')
+print (f'Page length: {len(res.text)}')
+
+html = res.text
+bs = BeautifulSoup(html, features='html.parser')
+
+for url in bs.find_all('img'):
+    download_image(url.get('src'))
